c7d97a1f5c747b4d920be6ec563728c27e195697
olivier <OlivierMoreau256@gmail.com>
2018-04-24 08:33:54 +0200

    modification de syntaxe et pré-débug

--- EndlessArena.py
+++ EndlessArena.py
@@ -1,67 +1,68 @@
-# -*- coding: utf-8 -*-
+#   -*- coding: utf-8 -*-
 
-# Les imports
-
-
-# Les variables globales
+#   Les imports
 
 
-# Les classes
+#   Les variables globales
+
+
+#   Les classes
 class Univers(object):
-    """La classe générale, qui gère l'initialisation, les autres objets et les contrôleurs"""
-    # Attributs
-    jeu = [] #la seule variable qui va voyager, contient la position de chaque objet sous la forme {nom, x, y}
-    
-    # Méthodes
+    """La classe générale, qui gère l'initialisation, les autres objets et les
+    contrôleurs"""
+    #   Attributs
+    jeu = []  # la seule variable qui va voyager, contient la position de chaque
+    # objet sous la forme {nom, x, y}
+
+    #   Méthodes
     def __init__(self):
         """ Initialisation """
         pass
-    
+
     def affichage(self):
         pass
-    
+
     def menus(self):
         pass
-    
+
     def checkChutePersos(self):
-        joueurs = [joueur1; joueur2]
-        for test in joueurs :
+        joueurs = [joueur1, joueur2]
+        for test in joueurs:
             if test.position_perso['y'] > tailleFenetreY:
                 Univers.compteurDePoints(joueurs.index(test), -1)
                 test.position_perso['y'] = positionInitialeY
                 test.position_perso['x'] = positionInitialeX
-    
+
     def checkMeurtrePersos(self):
         pass
-    
+
     def compteurDePoints(self, joueur, increment):
         joueur.position_perso['points'] += increment
 
 
 class Plateforme(object):
     """La classe qui gère les plateformes"""
-    
-    #Méthodes
+
+    #  Méthodes
     def __init__(self, name, posX, posY):
         self.nom = name
         self.position_perso = {'x': posX, 'y': posY}
-    
+
     def deplacement(self, deplacement):
         """Pour déplacer les plateformes, deplacement en pixel. deplacement > 0 ==> vers la droite ; deplacement < 0 ==> vers la gauche"""
         self.position_plateforme['y'] += deplacement
-    
 
     def jeu(self):
         pass
 
+
 class Personnage(object):
     """La classe qui gère les personnages"""
-    #Méthodes
+    #  Méthodes
 
     def __init__(self, name):
         self.nom = name
         self.position_perso = {'nom': self.nom, 'x': 1, 'y': 0, 'sens': 1}
-
 
     def deplacementDroite(self):
         self.position_perso['sens'] = 1
@@ -72,51 +73,49 @@ class Personnage(object):
         self.position_perso['x'] -= 1
 
     def saut(self):
-        self.position_perso['y'] += 5 ##trés mauvais, à améliorer
+        self.position_perso['y'] += 5 #  #  trés mauvais, à améliorer
 
-    
 
 class Epee(object):
     """La classe qui gère les épées"""
-    #Attributs
+    #  Attributs
 
-    #Méthodes
+    #  Méthodes
     def __init__(self, name):
         self.nom = name
         self.position_epee = {'nom': self.nom, 'x': maitre.get('x'), 'y': maitre.get('y')}
 
-
     def coup(self):
-        if maitre.sens == 1 : #si le maître est tourné vers la droite
+        if maitre.sens == 1 : #  si le maître est tourné vers la droite
             self.position_epee['x'] += 1
         else :
             self.position_epee['x'] += 1
 
+
 class Grappin(object):
     """La classe qui gère les grappins"""
 
-    #Attributs
-    maitre =  0 #Le personnage auquel l'épée est attachée
+    #  Attributs
+    maitre = 0  # Le personnage auquel l'épée est attachée
 
-    
-    #Méthodes
+    #  Méthodes
     def __init__(self, name, joueur, direction):
         self.nom = name
         self.joueur = joueur
         self.direction = direction
         self.etat = 0
-        self.position_grappin = {'x':largeurPerso/2;,'y':hauteurPerso/2}
-    
-    def principale (self):
-        if self.etat = 0 :
+        self.position_grappin = {'x': largeurPerso/2, 'y': hauteurPerso/2}
+
+    def principale(self):
+        if self.etat == 0:
             self.lance()
-        
-        if self.etat = 1 :
+
+        if self.etat == 1:
             self.tracte()
-    
-    def changement(self) :
-        self.direction = random.randint(0,8)
-    
+
+    def changement(self):
+        self.direction = random.randint(0, 8)
+
     def lance(self):
         if self.direction == 0:
             self.position_grappin['y'] += -5
@@ -138,21 +137,22 @@ class Grappin(object):
         if self.direction == 5:
             self.position_grappin['y'] += -3
             self.position_grappin['x'] += -3
-    
+
     def tracte(self):
         distance = math.hypot(self.pisition_grappin['x'], self.pisition_grappin['y']) - 5
-        if self.pisition_grappin['y'] - joueur.position_perso['y'] != 0 and self.pisition_grappin['x'] - joueur.position_perso['x'] != 0 :
-            angle = math.atan(self.pisition_grappin['y'] - joueur.position_perso['y']/ self.pisition_grappin['x'] - joueur.position_perso['x'])
-        else :
+        if self.pisition_grappin['y'] - joueur.position_perso['y'] != 0 and self.pisition_grappin['x'] - joueur.position_perso['x'] != 0:
+            angle = math.atan(self.pisition_grappin['y'] - joueur.position_perso['y']/self.pisition_grappin['x'] - joueur.position_perso['x'])
+        else:
             if self.pisition_grappin['y'] - joueur.position_perso['y'] > 0 :
-            
+
         joueur.position_perso['x'] = distance * math.cos(angle)
         joueur.position_perso['y'] = distance * math.sin(angle)
-    
+
     def tranche(self):
         self.destroy
-# Le programme principal
 
 
-#Univers(lesArguments)
+#   Le programme principal
 
+
+#  Univers(lesArguments)
